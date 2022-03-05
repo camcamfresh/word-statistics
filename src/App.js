2f827345fa6a4381bf5e79b0f08942e026463a3f
Cameron <camcamfresh@gmail.com>
2022-03-05 17:05:30 -0500

    Add textarea & bootstrap css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
 	return (
-		<>
+		<div className='d-flex flex-column h-100'>
 			<h1>Word Statistics</h1>
-		</>
+			<textarea
+				className='flex-fill flex-grow-1 mx-2 mb-2'
+				placeholder='Enter text to analyze'
+			></textarea>
+		</div>
 	);
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach(() => {
   render(<App />);
+})
+
+test('renders Word Statistics title', () => {
   const headingElement = screen.getByText('Word Statistics');
   expect(headingElement).toBeInTheDocument();
+});
+
+test('renders text area', () => {
+  const textareaElement = screen.getByPlaceholderText('Enter text to analyze');
+  expect(textareaElement).toBeInTheDocument();
 });
