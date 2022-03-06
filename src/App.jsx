183eeb3d03f1ce95b862257ebd5c9cc910303150
Cameron <camcamfresh@gmail.com>
2022-03-05 23:02:50 -0500

    Save textInput in App state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+	const [textInput, setTextInput] = useState('');
+
 	return (
 		<div className='d-flex flex-column h-100'>
 			<h1>Word Statistics</h1>
-			<div className='d-flex flex-row h-100'>
+			<div className='d-flex flex-row flex-fill overflow-auto'>
 				<textarea
-					className='flex-fill flex-grow-1 mx-2 mb-2'
+					className='flex-grow-1 mx-2 mb-2'
+					onInput={(event) => setTextInput(event.target.value)}
 					placeholder='Enter text to analyze'
+					value={textInput}
 				></textarea>
 				<div className='mx-4'>
 					<label>Word Frequency</label>
--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 beforeEach(() => {
   render(<App />);
 })
 
-test('renders Word Statistics title', () => {
+it('renders Word Statistics title', () => {
   const headingElement = screen.getByText('Word Statistics');
   expect(headingElement).toBeInTheDocument();
 });
 
-test('renders text area', () => {
+it('renders text area', () => {
   const textareaElement = screen.getByPlaceholderText('Enter text to analyze');
   expect(textareaElement).toBeInTheDocument();
 });
 
-test('renders word freqency area', () => {
+it('renders word freqency area', () => {
   const wordFreqencyElement = screen.getByText('Word Frequency');
 	expect(wordFreqencyElement).toBeInTheDocument();
 });
+
+it('updates textarea based on input', () => {
+  const textareaElement = screen.getByPlaceholderText('Enter text to analyze');
+  userEvent.click(textareaElement);
+  userEvent.type(textareaElement, 'Hello World');
+	expect(textareaElement.value).toEqual('Hello World');
+})
